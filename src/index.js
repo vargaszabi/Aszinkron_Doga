fa4cccfe210a056766dd7efa88f0075db7b6e1de
Varga Szabolcs <vargaszabi2001@gmail.com>
2022-11-15 14:21:48 +0100

    1.feladat kész

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css';
 
+let quotes = []
+
+document.addEventListener('DOMContentLoaded', async () => {
+    let response = await fetch('/quotes.json');
+    let result = await response.json();
+    for(let e of result.quotes) {
+        quotes.push(e);
+    }
+    document.getElementById('osszesidezet').addEventListener('click', async () => {
+        document.getElementById('list').textContent = "";
+        response = await fetch('/quotes.json');
+        result = await response.json();
+        for(let e of quotes) {
+            let li = document.createElement('li');
+            li.innerHTML = e.quote;
+            document.getElementById('list').appendChild(li);
+        }
+    })
+})
